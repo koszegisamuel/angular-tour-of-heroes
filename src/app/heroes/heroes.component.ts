07f09674c273af60b01e472a2b5d4bf715b3de1d
koszegisamuel <koszegi.samuel@gmail.com>
2023-01-03 18:54:27 +0100

    Added mock-heroes and display hero list with detai

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../_model/hero';
+import { HEROES } from '../_model/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent  implements OnInit{
+export class HeroesComponent implements OnInit {
   //itt megadott metódusok a komponens betöltésekor autamituksan meghívódnak
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-  
-  constructor() { }
-  
-  ngOnInit(): void {
-   
-  }
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
